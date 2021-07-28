e593b8c4246f67c0045d1a4754cc3f8fa3dc0a15
jason7531 <54910136+jason7531@users.noreply.github.com>
2021-07-28 22:32:51 +0530

    client side finalised and ready for submission

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,43 +1,6 @@
 import React, { Fragment } from "react";
 import useInput from "../hooks/use-input";
 const Login = () => {
-  const {
-    value: enteredEmail,
-    enteredValueIsValid: enteredEmailIsValid,
-    enteredValueHasError: emailInputHasError,
-    valueChangeHandler: emailInputChangeHandler,
-    valueInputBlurHandler: emailInputBlurHandler,
-    reset: emailReset,
-  } = useInput((value) => {
-    return value.trim() !== "";
-  });
-
-  const {
-    value: enteredPassword,
-    enteredValueIsValid: enteredPasswordIsValid,
-    enteredValueHasError: passwordInputHasError,
-    valueChangeHandler: passwordInputChangeHandler,
-    valueInputBlurHandler: passwordInputBlurHandler,
-    reset: passwordReset,
-  } = useInput((value) => {
-    return value.trim() !== "";
-  });
-
-  let formIsValid = false;
-
-  if (enteredPasswordIsValid && enteredEmailIsValid) {
-    formIsValid = true;
-  }
-
-  const formSubmissionHandler = (event) => {
-    event.preventDefault();
-    if (!formIsValid) {
-      return;
-    }
-    console.log(enteredEmail);
-    emailReset();
-    passwordReset();
-  };
   return (
     <Fragment>
       <div class="container h-100">
@@ -54,67 +17,16 @@ const Login = () => {
             </div>
             <div class="d-flex justify-content-center form_container">
               <form>
-                <div class="input-group mb-3">
-                  <div class="input-group-append">
-                    <span class="input-group-text">
-                      <i class="fas fa-user"></i>
-                    </span>
-                  </div>
-                  <input
-                    type="text"
-                    name=""
-                    class="form-control input_user"
-                    value=""
-                    placeholder="username"
-                  />
-                </div>
-                <div class="input-group mb-2">
-                  <div class="input-group-append">
-                    <span class="input-group-text">
-                      <i class="fas fa-key"></i>
-                    </span>
-                  </div>
-                  <input
-                    type="password"
-                    name=""
-                    class="form-control input_pass"
-                    value=""
-                    placeholder="password"
-                  />
-                </div>
-                <div class="form-group">
-                  <div class="custom-control custom-checkbox">
-                    <input
-                      type="checkbox"
-                      class="custom-control-input"
-                      id="customControlInline"
-                    />
-                    <label
-                      class="custom-control-label"
-                      for="customControlInline"
-                    >
-                      Remember me
-                    </label>
-                  </div>
-                </div>
                 <div class="d-flex justify-content-center mt-3 login_container">
-                  <button type="button" name="button" class="btn login_btn">
+                  <a
+                    href="http://localhost:4000/handleLogin"
+                    name="button"
+                    class="btn login_btn"
+                  >
                     Login
-                  </button>
+                  </a>
                 </div>
               </form>
-            </div>
-
-            <div class="mt-4">
-              <div class="d-flex justify-content-center links">
-                Don't have an account?{" "}
-                <a href="#" class="ml-2">
-                  Sign Up
-                </a>
-              </div>
-              <div class="d-flex justify-content-center links">
-                <a href="#">Forgot your password?</a>
-              </div>
             </div>
           </div>
         </div>
--- src/pages/Repos.js
+++ src/pages/Repos.js
@@ -1,18 +1,49 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import FolderIcon from "@material-ui/icons/Folder";
+
 const Repos = () => {
+  const [repoData, setRepoData] = useState([]);
+  const addRepos = async () => {
+    const response = await fetch("http://localhost:4000/index", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      const name = data.gh_username;
+      const repoRes = await fetch(`http://localhost:4000/user/${name}`, {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (repoRes.ok) {
+        const repoDatas = await repoRes.json();
+        console.log(repoDatas);
+        setRepoData(repoDatas.repo_info);
+        console.log(repoData);
+      }
+    }
+  };
+  useEffect(() => {
+    addRepos();
+  }, []);
   return (
     <Fragment>
-      <div class="card">
-        <div class="card-body">This is some text within a card body.</div>
-        <FolderIcon style={{ float: "right" }} />
-      </div>
-      <div class="card">
-        <div class="card-body">This is some text within a card body.</div>
-      </div>
-      <div class="card">
-        <div class="card-body">This is some text within a card body.</div>
-      </div>
+      {repoData &&
+        repoData.map((item) => {
+          return (
+            <div class="card">
+              <div class="card-body">{item.repo_name}</div>
+              <FolderIcon style={{ float: "right" }} />
+            </div>
+          );
+        })}
     </Fragment>
   );
 };
