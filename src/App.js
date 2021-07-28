688d1a16548f87c8c2b018fceec499497352b403
jason7531 <54910136+jason7531@users.noreply.github.com>
2021-07-28 11:54:40 +0530

    lazy loading added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Layout from "./components/layout/Layout";
-
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 import "./App.css";
 import { Route, Switch, Redirect } from "react-router-dom";
-import Login from "./pages/Login";
-import Repos from "./pages/Repos";
+const Login = React.lazy(() => import("./pages/Login"));
+const Repos = React.lazy(() => import("./pages/Repos"));
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/login" />
-        </Route>
-        <Route path="/login" exact>
-          <Login />
-        </Route>
-        <Route path="/repos" exact>
-          <Repos />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/login" />
+          </Route>
+          <Route path="/login" exact>
+            <Login />
+          </Route>
+          <Route path="/repos" exact>
+            <Repos />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
